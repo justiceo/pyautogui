97e7fdf672c1e4144f345af9c586ea51b0f94947
Justice Ogbonna <ebesco@yahoo.com>
2017-07-31 22:48:59 -0400

    apparently xlib wasn't installed by pip

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ sudo apt-get update
 
 sudo apt-get -y install python3-pip
 
-pip3 install python3-xlib
-
 sudo apt-get -y install scrot
 
 sudo apt-get -y install python3-tk
 
 sudo apt-get -y install python3-dev
+
+sudo apt-get -y install python3-xlib
+
+pip3 install pillow
 
 pip3 install pyautogui
